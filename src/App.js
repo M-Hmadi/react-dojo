0160a7ba51177d74d03fa5928cb8ef7250f2284d
M-Hmadi <moh.hamadi98@gmail.com>
2023-02-09 19:06:26 +0200

    Added home and navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
